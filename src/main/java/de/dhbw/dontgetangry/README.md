4c3b3f19aa1057804e6ae9474fbfbe2475deb1c1
Lukas Holler <lukas.holler@live.de>
2023-06-04 23:37:59 +0200

    added README.md

---
+++ src/main/java/de/dhbw/dontgetangry/README.md
@@ -0,0 +1,19 @@
+# DontGetAngryP2p
+
+## Start der Anwendung
+- Lade zunächst mit `mvn clean install` alle dependencys des Projekts herunter.
+- Starte anschließend mit einer IDE deiner Wahl die Main Klasse.
+- Neues Spiel Starten und dabei einen von der Firewall geöffneten Port angeben (Bspw. Port 80)
+- Auf zweitem Client die IP des SessionLeaders & Port angeben.
+
+## Funktionen der Anwendung
+- Spielen mit 2-4 Spielern
+- P2P Anwendung welche ein Netzwerk mit einer vollvermaschten Topologie aufbaut
+- Es gibt keinen dedizierten Server
+
+## Einschänkungen
+- Zum Testen der Anwendung sind 2 Computer nötig, da die Anwendung
+- Die Anwendung sollte mit einer minimalen Bidschirmauflösung von 1080x1920 betrieben werden.
+- Das Würfeln findet nicht dezentral statt. Schummeln wäre durch Code-Manipulation möglich.
+
+
--- src/test/java/de/dhbw/dontgetangry/GameUpdateTest.java
+++ src/test/java/de/dhbw/dontgetangry/GameUpdateTest.java
@@ -1,61 +1,61 @@
-package de.dhbw.dontgetangry;
-
-import de.dhbw.dontgetangry.model.Player;
-import de.dhbw.dontgetangry.netty.GameClient;
-import de.dhbw.dontgetangry.netty.GameServer;
-import de.dhbw.dontgetangry.netty.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static de.dhbw.dontgetangry.netty.GameProtocolKeywords.DiceRolled;
-
-public class GameUpdateTest {
-
-    public static void main(String[] args) throws Exception {
-        int port = 5000;
-        String serverHost = "localhost";
-        GameConnectionsMgr gameConnectionsMgr = new GameConnectionsMgr();
-
-
-        // only for testing (to use .sleep()) not needed in real game
-        Thread serverThread = new Thread(() -> {
-            try {
-
-                gameConnectionsMgr.start(Player.getPlayerById(0), port);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-        serverThread.start();
-
-        serverThread.sleep(2000); // Wait for the server to start
-        // Create and start multiple clients
-        List<GameClient> clients = new ArrayList<>();
-        clients.add(new GameClient(Player.RED));
-        clients.add(new GameClient(Player.BLUE));
-        clients.add(new GameClient(Player.GREEN));
-
-        for (GameClient client : clients) {
-            Thread clientThread = new Thread(() -> {
-                try {
-                    client.sendUpdate(DiceRolled.getKeyword() + "/" + client.getPlayer() + ";" + 6, serverHost, port);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
-            clientThread.start();
-        }
-
-        // Shutdown the clients
-        for (GameClient client : clients) {
-            client.stop();
-        }
-
-        // Shutdown the server
-        serverThread.sleep(2000);
-        gameConnectionsMgr.getGameServer().stop();
-    }
-}
-
+//package de.dhbw.dontgetangry;
+//
+//import de.dhbw.dontgetangry.model.Player;
+//import de.dhbw.dontgetangry.netty.GameClient;
+//import de.dhbw.dontgetangry.netty.GameServer;
+//import de.dhbw.dontgetangry.netty.*;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import static de.dhbw.dontgetangry.netty.GameProtocolKeywords.DiceRolled;
+//
+//public class GameUpdateTest {
+//
+//    public static void main(String[] args) throws Exception {
+//        int port = 5000;
+//        String serverHost = "localhost";
+//        GameConnectionsMgr gameConnectionsMgr = new GameConnectionsMgr();
+//
+//
+//        // only for testing (to use .sleep()) not needed in real game
+//        Thread serverThread = new Thread(() -> {
+//            try {
+//
+//                gameConnectionsMgr.start(Player.getPlayerById(0), port);
+//
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        });
+//        serverThread.start();
+//
+//        serverThread.sleep(2000); // Wait for the server to start
+//        // Create and start multiple clients
+//        List<GameClient> clients = new ArrayList<>();
+//        clients.add(new GameClient(Player.RED));
+//        clients.add(new GameClient(Player.BLUE));
+//        clients.add(new GameClient(Player.GREEN));
+//
+//        for (GameClient client : clients) {
+//            Thread clientThread = new Thread(() -> {
+//                try {
+//                    client.sendUpdate(DiceRolled.getKeyword() + "/" + client.getPlayer() + ";" + 6, serverHost, port);
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            });
+//            clientThread.start();
+//        }
+//
+//        // Shutdown the clients
+//        for (GameClient client : clients) {
+//            client.stop();
+//        }
+//
+//        // Shutdown the server
+//        serverThread.sleep(2000);
+//        gameConnectionsMgr.getGameServer().stop();
+//    }
+//}
+//
